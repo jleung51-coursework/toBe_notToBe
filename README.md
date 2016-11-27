1bd5fd21b4990fb587503cef4f7a8fc9e92fa866
Jeffrey Leung <jleung513@gmail.com>
2016-11-26 20:25:06 -0800

    Add parody code and README

---
+++ README.md
@@ -0,0 +1,7 @@
+# To Be, or Not To Be
+
+Java "program" parodying Hamlet's "To be, or not to be" speech, written for an English 12 assignment.
+
+No, it is not actually an executable Java program, nor was it ever intended to be.
+
+I honestly don't know what else to say.
---
+++ to_be.java
@@ -0,0 +1,83 @@
+//Welcome to Java
+//Code is implemented from top to bottom
+//Code comments (text which is not run by the program) are preceded by two forward slashes
+//Blocks of code are sorted by indents and curly brackets
+//Code is not guaranteed to run without terrible errors
+
+public class HamletByShakespeare
+{
+
+	toBe = true;
+	notToBe = false;
+
+	//The || is the OR operator
+	question = toBe || notToBe;
+
+	//If suffering equals being noble, run the following code in the brackets
+	if (nobler == sufferingTheSlingsAndArrowsOfOutrageousFortune)
+	{
+		toBe = true;
+		notToBE = false;
+	}
+
+	else if (smartDecision == takeUpArmsAgainstASeaOfTroubles)
+	{
+		toBe = false;
+		notToBE = true;
+		
+		if (opposition == true)
+		{
+			troubles = 0;
+			die = sleep = true;
+
+			if (randomNum == 0)
+			{
+				dream = true;
+			}
+		}
+	}
+
+	rub = found;
+
+	//The && is the AND operator, to check if both statements are true
+	if (sleepOfDeath == true && terribleDreams == inTheSleep)
+	{
+		pause = true;
+	}
+
+	find (whoWouldBear)
+	{
+		whipsAndScornsOfTime;
+		oppressorsWrong;
+		proudMansContumely;
+		pangsOfDespisedLove;
+		lawsDelay;
+		insolenceOfOffice;
+		spurnsThatPatientMeritOfTheUnworthyTakes;
+
+		toBe = true;
+		notToBe = false;
+	}
+
+	if (choice == wearyLife)
+	{
+		dreadOfSomethingAfterDeath = true;
+		run evilsOfLife();
+		evilsOfDeath = future;
+
+		toBe = true;
+		notToBe = false;
+	}
+
+	if (conscience == true)
+	{
+		everyone = coward;
+	}
+
+	if (toBe == true)
+	{
+		enterprisesOfGreatPithAndMoment = awry;
+		action = 0;
+		Hamlet = alive;
+	}
+}
